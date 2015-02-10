255ea5cdcbe948d3c10f6520a0ddbdb3207ff20f
miaozhirui <miaozhirui@patsnap.com>
2015-02-10 19:31:50 +0800

    练习设计模式

--- js/designPattern/factory_pattern.php
+++ js/designPattern/factory_pattern.php
@@ -3,6 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <title>工厂模式</title>
+    <!-- 什么时候时候工厂模式
+            1.对象的构建十分复杂
+            2.需要依赖具体环境创建不同实例
+            3.处理大量具有相同属性的小对象
+     -->
 </head>
 <body>
     <script type="text/javascript">
@@ -21,11 +26,38 @@
     }
     // product.factory('createB')
     
-    // 实例二
+    // 实例二(有这样的需求，页面不确定要插入文本，链接，图片)
     var page = page || {}
-    var page.dom = page.dom || {}
+    page.dom = page.dom || {}
 
-    
+    page.dom['text'] = function() {
+        this.insert = function(where) {
+            var txt = document.createTextNode(this.url);
+            where.appendChild(txt);
+        }
+    }
+    page.dom['link'] = function() {
+        this.insert = function(where) {
+            var link = document.createElement('a');
+            link.href = this.url;
+            link.appendChild(document.createTextNode(this.url));
+            where.appendChild(link)
+        }
+    }
+    page.dom['img'] = function() {
+        this.insert = function(where) {
+            var img = document.createElement('img');
+            img.src = this.url;
+            where.appendChild(img)
+        }
+    }
+    page.dom.factory = function(type) {
+        return new page.dom[type];
+    }
+
+    var obj = page.dom.factory('img');
+    obj.url = 'http://www.baidu.com';
+    obj.insert(document.body);
     </script>
 </body>
 </html>
